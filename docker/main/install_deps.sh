3b02c5ba17cde7e9d062061139fc683437abb57d
nyok92 <40342954+nyok92@users.noreply.github.com>
2026-07-24 18:32:00 +0200

    Update install_deps.sh

--- docker/main/install_deps.sh
+++ docker/main/install_deps.sh
@@ -84,7 +84,10 @@ if [[ "${TARGETARCH}" == "arm64" ]]; then
     rm -f ffmpeg.tar.xz
     mkdir -p /usr/lib/ffmpeg/9.0
     wget -qO ffmpeg.zip "https://github.com/jimmyhon/frigate-builds/releases/download/ffmpeg-8.0.1/ffmpeg.zip"
-    unzip ffmpeg.zip -C /usr/lib/ffmpeg/9.0
+    unzip ffmpeg.zip 
+    cd ffmpeg
+    cp -r . /usr/lib/ffmpeg/9.0
+    cd ..
     rm -rf ffmpeg.zip
 fi
 
