be45da4d12a13b9fcf1519e13d9dc0e1acea732f
nyok92 <40342954+nyok92@users.noreply.github.com>
2026-07-26 08:20:50 +0200

    Update install_deps.sh

--- docker/main/install_deps.sh
+++ docker/main/install_deps.sh
@@ -83,11 +83,11 @@ if [[ "${TARGETARCH}" == "arm64" ]]; then
     wget -qO ffmpeg.tar.xz "https://github.com/NickM-27/FFmpeg-Builds/releases/download/autobuild-2026-06-02-14-20/ffmpeg-n8.1.1-9-g58d4114d36-linuxarm64-gpl-8.1.tar.xz"
     tar -xf ffmpeg.tar.xz -C /usr/lib/ffmpeg/8.0 --strip-components 1 arm64/bin/ffmpeg arm64/bin/ffprobe
     rm -f ffmpeg.tar.xz
-    mkdir -p /config/custom_ffmpeg
+    mkdir -p /config/custom_ffmpeg/bin
     wget -qO ffmpeg.zip "https://github.com/jimmyhon/frigate-builds/releases/download/ffmpeg-8.0.1/ffmpeg.zip"
     unzip ffmpeg.zip 
-    mv ffmpeg /config/custom_ffmpeg/ffmpeg
-    mv ffprobe /config/custom_ffmpeg/ffprobe
+    mv ffmpeg /config/custom_ffmpeg/bin/ffmpeg
+    mv ffprobe /config/custom_ffmpeg/bin/ffprobe
     rm -rf ffmpeg.zip
 fi
 
